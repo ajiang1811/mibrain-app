ee75bb90b9ce9fc627bd1bd20cf0c88da203cdeb
ajiang1811 <89481585+ajiang1811@users.noreply.github.com>
2024-10-23 13:11:23 -0400

    Views, Apps

--- mibrain/quickstart/apps.py
+++ mibrain/quickstart/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
 
+from django.urls import include, path
+from rest_framework import routers
 
-class QuickstartConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quickstart'
+from tutorial.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
